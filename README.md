5617f258e33a0fa811649ec007187f9b759ebc2a
igor.gorini <igor.gorini@elotech.com.br>
2022-06-27 22:34:08 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PROVA
+
+chmod +x mvnw
+
+./mvnw spring-boot:run
---
+++ src/main/java/br/edu/unicesumar/prova/amqp/RabbitStartup.java
@@ -0,0 +1,28 @@
+package br.edu.unicesumar.prova.amqp;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Binding.DestinationType;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitStartup {
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    @PostConstruct
+    private void postConstruct() {
+
+    }
+
+}
